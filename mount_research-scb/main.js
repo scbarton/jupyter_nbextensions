a735bc53a36de10d48a1226b93fd77452c924eba
Scott Calabrese Barton <scb@msu.edu>
2019-08-31 13:12:56 -0400

    replace scb with EGRUSER

--- mount_research-scb/main.js
+++ mount_research-scb/main.js
@@ -5,7 +5,7 @@ function(Jupyter) {
 
     var insert_mnt_cell = function ()  {    
         
-    var text='%%bash\n\n# If you have not already done so, set up public key login between this machine and scully \n# (from a terminal on this machine): \n# > ssh-keygen \n# > ssh-copy-id egruser@scully.egr.msu.edu\n\n# Replace EGRUSER with your EGR account information. \n# Please delete after running  \n\nmkdir -p ~/mnt/research-scb\nsshfs scb@scully.egr.msu.edu:/egr/research-scb ~/mnt/research-scb -o volname=research-scb'
+    var text='%%bash\n\n# If you have not already done so, set up public key login between this machine and scully \n# (from a terminal on this machine): \n# > ssh-keygen \n# > ssh-copy-id egruser@scully.egr.msu.edu\n\n# Replace EGRUSER with your EGR account information. \n# Please delete after running  \n\nmkdir -p ~/mnt/research-scb\nsshfs EGRUSER@scully.egr.msu.edu:/egr/research-scb ~/mnt/research-scb -o volname=research-scb'
 
     Jupyter.notebook.insert_cell_above('code', 0).set_text(text)
 
